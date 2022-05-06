81e20ea57dd86922d1b6fb7e2c51f266c8301928
Steve Pikelny <spike716@gmail.com>
2022-05-06 15:34:41 -0400

    blah

---
+++ hardhat.config.js
@@ -0,0 +1,44 @@
+
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-web3");
+
+
+const STUFF = require('../DEV_KEYS/stuff.json')
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {version: "0.8.0"},
+      {version: "0.8.11"},
+    ]
+  },
+  networks: {
+    mumbai: {
+      url: STUFF.POLYGON_MUMBAI_URL,
+      accounts: [`0x${STUFF.DEV1}`]
+    },
+    rinkeby: {
+      url: STUFF.ETH_RINKEBY_URL,
+      accounts: [`0x${STUFF.DEV1}`]
+    },
+    goerli: {
+      url: STUFF.ETH_GOERLI_URL,
+      accounts: [`0x${STUFF.DEV1}`]
+    },
+
+
+    // hardhat: {
+    //   mining: {
+    //     auto: false,
+    //     interval: 5000
+    //   }
+    // },
+
+  },
+
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 1000
+  }
+}
---
+++ test/uncirculatedTest.js
@@ -0,0 +1,84 @@
+
+const { expect } = require("chai")
+
+const expectFailure = async (fn, err) => {
+  let failure
+  try {
+    await fn()
+  } catch (e) {
+    failure = e
+  }
+  expect(failure.message).to.include(err)
+}
+
+const num = n => Number(ethers.utils.formatEther(n))
+
+describe('UncirculatedFakeInternetMoney', () => {
+  it('should work', async () => {
+    const [owner, buyer1, buyer2] = await ethers.getSigners()
+
+    const UncirculatedFactory = await ethers.getContractFactory('UncirculatedFakeInternetMoney', owner)
+    Uncirculated = await UncirculatedFactory.deploy()
+    await Uncirculated.deployed()
+    await Uncirculated.connect(owner).mint(owner.address, 0)
+    await Uncirculated.connect(owner).mint(owner.address, 20)
+    await Uncirculated.connect(owner).mint(owner.address, 21)
+    await Uncirculated.connect(owner).mint(owner.address, 22)
+    await Uncirculated.connect(owner).mint(owner.address, 23)
+    await Uncirculated.connect(owner).mint(owner.address, 24)
+
+    const TokenURIFactory = await ethers.getContractFactory('TokenURI', owner)
+    const TokenURI = await TokenURIFactory.deploy(Uncirculated.address)
+    await TokenURI.deployed()
+
+
+    const MinterFactory = await ethers.getContractFactory('Minter', owner)
+    Minter = await MinterFactory.deploy(Uncirculated.address)
+    await Minter.deployed()
+
+    await Minter.connect(owner).flipIsLocked()
+
+
+    await Uncirculated.connect(owner).setTokenURIPointer(TokenURI.address)
+    await Uncirculated.connect(owner).setMintingAddress(Minter.address)
+
+    const baseMetadata = {
+      baseImgUrl: 'ipfs://abcd/',
+      imgExtension: '.jpeg',
+      baseExternalUrl: 'https://uncirculatedmoney.com/',
+      license: 'CC BY-NC 4.0',
+      description: 'Uncirculated FIM'
+    }
+    await TokenURI.connect(owner).setBaseMetadata(
+      baseMetadata.baseImgUrl,
+      baseMetadata.imgExtension,
+      baseMetadata.baseExternalUrl,
+      baseMetadata.license,
+      baseMetadata.description,
+    )
+
+    const startingOwnerBalance = num(await owner.getBalance())
+
+    await Minter.connect(buyer1).mint(1, {
+      value: ethers.utils.parseEther('0.0995')
+    })
+    await Minter.connect(buyer2).mint(2, {
+      value: ethers.utils.parseEther('0.0995')
+    })
+
+    expect(await Uncirculated.connect(owner).ownerOf(1)).to.equal(buyer1.address)
+    expect(await Uncirculated.connect(owner).ownerOf(2)).to.equal(buyer2.address)
+
+    await expectFailure(() =>
+      Minter.connect(buyer2).withdraw(),
+      'Ownable:'
+    )
+
+    await Minter.connect(owner).withdraw()
+
+    const endingOwnerBalance = num(await owner.getBalance())
+
+    expect(endingOwnerBalance - startingOwnerBalance).to.be.closeTo(0.0995*2, 0.001)
+
+  })
+})
